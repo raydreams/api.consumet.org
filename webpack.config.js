758b855ef006f3f8dcc6a3fa49f398108f22497f
theo <dev@starnode.host>
2025-04-29 14:23:57 -0400

    Fix issues with build (#5)

--- webpack.config.js
+++ webpack.config.js
@@ -6,3 +6,24 @@ if (typeof window === 'undefined') {
   // Browser environment
   // Use browser-compatible alternatives
 }
+
+module.exports = {
+  resolve: {
+    fallback: {
+      fs: false,
+      path: false,
+      crypto: false,
+      stream: false,
+      util: false,
+      buffer: false,
+      process: false,
+      os: false,
+      zlib: false,
+      http: false,
+      https: false,
+      url: false,
+      assert: false,
+      querystring: false,
+    }
+  }
+};
